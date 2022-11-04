ec046cc16a3d3adc1bcba56465fd335abe9917cd
jonathan1q2w3e4r <jonathanlevy@gmail.com>
2022-11-04 14:56:01 -0400

    Fixed generator bug and added .gt extension to txt

--- trainingDataGenerator.py
+++ trainingDataGenerator.py
@@ -6,6 +6,7 @@ from pynput.mouse import Button, Controller
 from PIL import Image, ImageGrab
 
 TRAIN_ON_ITALICS = True
+NUMBER_OF_ITERATIONS = 1
 # letters and numbers and characters of my choosing
 letters = f'{string.ascii_letters}{string.digits}!@:,'
 buffer = 'abcdefghijklmnopqrstuvwxyzabcdefghijklmnopq'
@@ -15,32 +16,30 @@ mouse.position = (1200, 700)
 time.sleep(0.2)
 mouse.click(Button.left)
 keyboard = keyboard.Controller()
-for i in range(0, 1):
+for i in range(0, NUMBER_OF_ITERATIONS):
     sentence = ''
     # Resets random seed every iteration
     keyboard.press('/')
     # random sleep times due to paranoia
     if (TRAIN_ON_ITALICS):
-        time.sleep(random.randint(1000, 3000)/48000)
+        time.sleep(random.randint(500, 2000)/48000)
         keyboard.press('m')
-        time.sleep(random.randint(1000, 3000)/48000)
+        time.sleep(random.randint(500, 2000)/48000)
         keyboard.press('e')
-        time.sleep(random.randint(1000, 3000)/48000)
+        time.sleep(random.randint(500, 2000)/48000)
         keyboard.press(' ')
-
+    random.seed(round(time.time()*1000))
     for i in range(random.randint(1, 4)):
-        random.seed(round(time.time()*1000))
-        for j in range(random.randint(1, 12)):
-            time.sleep(0.01)
+        for j in range(random.randint(1, 10)):
             sentence += random.choice(letters)
         sentence += ' '
     for i in buffer:
         keyboard.press(i)
-        time.sleep(random.randint(1000, 2000)/48000)
+        time.sleep(random.randint(500, 2000)/48000)
         keyboard.release(i)
     for i in sentence:
         keyboard.press(i)
-        time.sleep(random.randint(1000, 2000)/48000)
+        time.sleep(random.randint(500, 2000)/48000)
         keyboard.release(i)
     keyboard.press(keyboard._Key.enter)
     time.sleep(random.randint(100, 200)/100)
