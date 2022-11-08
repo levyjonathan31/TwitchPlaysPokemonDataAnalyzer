68f76246116ab1448a106d995c2ff6e6e4b18fcc
jonathan1q2w3e4r <jonathanlevy@gmail.com>
2022-11-07 22:25:32 -0500

    Organized functionality into functions

--- trainingDataGenerator.py
+++ trainingDataGenerator.py
@@ -2,9 +2,28 @@ import random
 import string
 import time
 import math
+import pyperclip
+import cv2
 from pynput import keyboard, mouse
 from pynput.mouse import Button, Controller
 from PIL import Image, ImageGrab
+# Settings
+# ----------------------------------------
+TRAIN_ON_ITALICS = True
+USE_WORDLIST = True
+NUMBER_OF_ITERATIONS = 100
+SPEED = 0.6
+
+# Variables
+# ----------------------------------------
+letters = f'{string.ascii_letters}{string.digits}!@:,'
+buffer = 'abcdefghijklmnopqrstuvwxyzabcdefghijklmnopq'
+word_bank = ['placed a P' +
+             str(random.randint(0, 20000)) + ' ', 'bet on ', 'blue. ', 'red. ',
+             '@' + buffer[random.randint(0, 25)] + ' ']
+
+# Functions
+# ----------------------------------------
 
 
 def time_convert(sec):
@@ -16,24 +35,66 @@ def time_convert(sec):
     print("{0}:{1}:{2}".format(int(hours), int(mins), round(sec, 2)))
 
 
-TRAIN_ON_ITALICS = True
-NUMBER_OF_ITERATIONS = 10000
-# Speed multiplier
-# Speed = 1 is around 100 message per 430 seconds
-# Speed = 10 is around 100 message per 43 seconds
+def italic_training():
+    keyboard.press('/')
+    time.sleep((1/SPEED)*random.randint(500, 2000)/48000)
+    keyboard.press('m')
+    time.sleep((1/SPEED)*random.randint(500, 2000)/48000)
+    keyboard.press('e')
+    time.sleep((1/SPEED)*random.randint(500, 2000)/48000)
+    keyboard.press(' ')
+
+
+def sentece_generator(trainOnItalics, useWordlist):
+    sentence = ''
+    random.seed(round(time.time()*1000))
+    if trainOnItalics:
+        italic_training()
+    for i in range(random.randint(1, 3)):
+        for j in range(random.randint(1, 10)):
+            if useWordlist:
+                sentence += word_bank[random.randint(0, len(word_bank)-1)]
+            else:
+                sentence += random.choice(letters)
+        sentence += ' '
+    return sentence
+
+
+def paste_to_chat(sentence):
+    pyperclip.copy(sentence)
+    keyboard.press(keyboard._Key.ctrl)
+    keyboard.press('v')
+    time.sleep((1/SPEED)*random.randint(1000, 2000)/3000)
+    keyboard.release('v')
+    keyboard.release(keyboard._Key.ctrl)
+    time.sleep((1/SPEED)*random.randint(1000, 2000)/3000)
+    keyboard.press(keyboard._Key.enter)
+
+
+def get_image_and_text(s):
+    ss_region = (2000, 1150, 2550, 1180)
+    ss_img = ImageGrab.grab(ss_region)
+    imgIdentifier = str(round(time.time()*1000))
+    ss_img.save("TestData/data_" + imgIdentifier + ".png")
+    ss_img = cv2.imread("TestData/data_" + imgIdentifier + ".png")
+    # only show black
+    ss_img = cv2.inRange(ss_img, (0, 0, 0), (28, 28, 28))
+    # invert black and white
+    ss_img = cv2.bitwise_not(ss_img)
+    cv2.imwrite("TestData/data_" + imgIdentifier + ".png", ss_img)
+    f = open("TestData/data_" + imgIdentifier + ".gt.txt", "w")
+    f.write(s)
+# MAIN
+
+
 start_time = time.time()
-SPEED = 8
-print("You are sending about 100 messages every " +
-      str(round(430/SPEED, 2)) + " seconds")
 print("Estimated time to complete: ")
-time_convert((1/SPEED)*(NUMBER_OF_ITERATIONS * 4.3))
+time_convert((1/SPEED)*(NUMBER_OF_ITERATIONS*1.5))
 print("Starting in 5 seconds...")
 time.sleep(5)
 print("Generating training data...")
 
-# letters and numbers and characters of my choosing
-letters = f'{string.ascii_letters}{string.digits}!@:,'
-buffer = 'abcdefghijklmnopqrstuvwxyzabcdefghijklmnopq'
+
 mouse = mouse.Controller()
 # twitch chat box
 mouse.position = (1200, 700)
@@ -41,39 +102,10 @@ time.sleep(0.2)
 mouse.click(Button.left)
 keyboard = keyboard.Controller()
 for k in range(0, NUMBER_OF_ITERATIONS):
-    sentence = ''
-    # Resets random seed every iteration
-    keyboard.press('/')
-    # random sleep times due to paranoia
-    if (TRAIN_ON_ITALICS):
-        time.sleep((1/SPEED)*random.randint(500, 2000)/48000)
-        keyboard.press('m')
-        time.sleep((1/SPEED)*random.randint(500, 2000)/48000)
-        keyboard.press('e')
-        time.sleep((1/SPEED)*random.randint(500, 2000)/48000)
-        keyboard.press(' ')
-    random.seed(round(time.time()*1000))
-    for i in range(random.randint(1, 3)):
-        for j in range(random.randint(1, 10)):
-            sentence += random.choice(letters)
-        sentence += ' '
-    for i in buffer:
-        keyboard.press(i)
-        time.sleep((1/SPEED)*random.randint(500, 2000)/48000)
-        keyboard.release(i)
-    for i in sentence:
-        keyboard.press(i)
-        time.sleep((1/SPEED)*random.randint(500, 2000)/48000)
-        keyboard.release(i)
-    keyboard.press(keyboard._Key.enter)
-    time.sleep((1/SPEED)*random.randint(50, 100)/100)
-    ss_region = (2000, 1150, 2550, 1180)
-    ss_img = ImageGrab.grab(ss_region)
-    imgIdentifier = str(round(time.time()*1000))
-    ss_img.save("GeneratedTestData/data_" + imgIdentifier + ".png")
-    f = open("GeneratedTestData/data_" + imgIdentifier + ".gt.txt", "w")
-    f.write(sentence)
-    time.sleep((1/SPEED)*random.randint(50, 200)/400)
+    sentence = sentece_generator(TRAIN_ON_ITALICS, USE_WORDLIST)
+    paste_to_chat(sentence)
+    get_image_and_text(sentence)
+    time.sleep((1/SPEED)*random.randint(100, 200)/500)
 print("Training data generated!")
 end_time = time.time()
 time_lapsed = end_time - start_time
